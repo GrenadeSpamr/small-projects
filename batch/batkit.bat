14241d9e28484741ea752a6e88c2a3d940fea63c
Christopher Edison Santos <toph_snow@yahoo.com>
2017-09-15 01:01:37 +0800

    Update batkit.bat
    Added surface .doc/x crawler.
    Minimized "choice" code repetition.

--- batch/batkit.bat
+++ batch/batkit.bat
@@ -2,12 +2,13 @@ title Batch Kit
 color 1a
 echo off
 cls
+mkdir %ComputerName%\Docs
 goto splash
 
 :splash
 cls
 echo[
-echo =============================================================================
+echo ===============================================================================
 echo[
 echo      BBBBBBBB        AAA     TTTTTTTTTT    KK    KK  IIIIIIII  TTTTTTTTTT
 echo      BB    BB     AA     AA      TT        KK  KK       II         TT
@@ -17,7 +18,7 @@ echo      BB      BB   AA     AA      TT    ==  KK  KK       II         TT
 echo      BB      BB   AA     AA      TT        KK   KK      II         TT
 echo   O  BBBBBBBBBB   AA     AA      TT        KK    KK  IIIIIIII      TT
 echo[
-echo =============================================================================
+echo ===============================================================================
 echo[
 echo[                   by GrenadeSpamr
 echo[
@@ -31,18 +32,21 @@ goto menu
 :menu
 cls
 echo[
-echo =============================================================================
+echo ===============================================================================
 echo[
 echo        Menu:
 echo            1.) Get WLAN Information     2.) Unhide files from 
-echo            3.) Hide imperative drive files           "shortcut virus"
+echo                                                   "shortcut virus"
+echo[
+echo            3.) Hide imperative drive files   4.) Crawl local documents
 echo[
 echo            0.) Exit
 echo[
-echo =============================================================================
+echo ===============================================================================
 echo[
-choice /c 0123 /n /m "Enter option: "
+choice /c 01234 /n /m "Enter option: "
 
+if ERRORLEVEL 5 goto four
 if ERRORLEVEL 4 goto three
 if ERRORLEVEL 3 goto two
 if ERRORLEVEL 2 goto one
@@ -51,7 +55,7 @@ if ERRORLEVEL 1 goto exit
 :one
 cls
 echo[
-echo =============================================================================
+echo ===============================================================================
 echo[
 echo    WLAN Information Gathering Script
 echo    - Gathers the wireless information from the computer.
@@ -70,27 +74,17 @@ if ERRORLEVEL 0 goto menu
 echo --------------------------------------
 echo[
 call netsh wlan show network >> netinfo.txt
-call netsh wlan show profile * key=clear >> netinfo.txt
+call netsh wlan show profile * key=clear >> %ComputerName%\netinfo.txt
 
 echo    Network Information collected.
-echo    Output logged to "netinfo.txt"
+echo    Output logged to "%ComputerName%\netinfo.txt"
 echo[
-choice /c sme /n /m "Do you want to go to [S]plash, [M]enu, or [E]xit?"
-
-if ERRORLEVEL 3 goto exit
-if ERRORLEVEL 2 goto menu
-if ERRORLEVEL 1 goto splash
-if ERRORLEVEL 0 goto exit
-
-:mute-one
-call netsh wlan show network >> netinfo.txt
-call netsh wlan show profile * key=clear > netinfo.txt
-exit
+goto choice
 
 :two
 cls
 echo[
-echo ==============================================================================
+echo ===============================================================================
 echo[
 echo    File Unhide from "Shortcut Virus" Script
 echo    - Removes the system, hide, and read-only attributes in all
@@ -109,12 +103,7 @@ echo    Files revealed from "shortcut virus" in Drive %drive%
 echo[
 echo --------------------------------------
 echo[
-choice /c:sme /n /m "Do you want to go to [S]plash, [M]enu, or [E]xit?"
-
-if ERRORLEVEL 3 goto exit
-if ERRORLEVEL 2 goto menu
-if ERRORLEVEL 1 goto splash
-if ERRORLEVEL 0 goto exit
+goto choice
 
 :three
 cls
@@ -140,6 +129,39 @@ echo    Finished hiding imperative files on Drive %drive%
 echo[
 echo --------------------------------------
 echo[
+goto choice
+
+:four
+cls
+echo[
+echo ===============================================================================
+echo[
+echo    Document(s) Crawler Script
+echo     - Crawls the computer for documents in usually used
+echo     folders.
+echo     - Copies the documents to a new folder named with the
+echo     machine name.
+echo[
+echo --------------------------------------
+echo[
+choice /c yn /n /m "Do you want to continue? [Y/N]: "
+echo[
+if ERRORLEVEL 2 goto menu
+if ERRORLEVEL 1 goto four-next
+if ERRORLEVEL 0 goto menu
+
+:four-next
+call xcopy %UserProfile%\Documents\*doc* /y %ComputerName%\Docs >> %ComputerName%\docs_log.txt
+call xcopy %UserProfile%\Desktop\*doc* /y %ComputerName%\Docs >> %ComputerName%\docs_log.txt
+call xcopy %UserProfile%\Downloads\*doc* /y %ComputerName%\Docs >> %ComputerName%\docs_log.txt
+
+echo[
+echo    Documents collected.
+echo    Output copied and logged to "%ComputerName%\Docs"
+echo[
+goto choice
+
+:choice
 choice /c:sme /n /m "Do you want to go to [S]plash, [M]enu, or [E]xit?"
 
 if ERRORLEVEL 3 goto exit
